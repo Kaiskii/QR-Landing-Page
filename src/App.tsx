688c3a65f0364ddeb6ce960a78462f1e4e058979
Kaiskii <24761071+Kaiskii@users.noreply.github.com>
2024-02-25 12:01:34 +0800

    feat: implement qr component

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import QRCode from './assets/image-qr-code.png'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div className='flex flex-col justify-start items-center gap-5 bg-white rounded-2xl p-6 m-4 w-[22rem] h-[34rem]'>
+          <img src={QRCode} alt="" className='size-80 rounded-2xl' />
+
+          <p className='text-gray-900 font-bold font-outfit text-2xl'>
+            Improve your front-end skills by building projects
+          </p>
+
+          <p className='text-gray-400 font-outfit leading-5 w-72'>          
+            Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+          </p>
+        </div>
+
+        <div className="attribution text-slate-500">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+          Coded by <a href="#">Kaiskii</a>.
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'grayish-blue': '#675E4C',
+      },
+      fontFamily: {
+        outfit: ['"Outfit"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
