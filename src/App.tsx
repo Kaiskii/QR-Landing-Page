23e42eb3b258abf15eabb0e9e917f434f25a12dd
Kaiskii <24761071+Kaiskii@users.noreply.github.com>
2024-02-25 12:05:25 +0800

    chore: prettier format

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,33 @@
-import './App.css'
-import QRCode from './assets/image-qr-code.png'
+import './App.css';
+import QRCode from './assets/image-qr-code.png';
 
 function App() {
   return (
     <>
       <div>
-        <div className='flex flex-col justify-start items-center gap-5 bg-white rounded-2xl p-6 m-4 w-[22rem] h-[34rem]'>
-          <img src={QRCode} alt="" className='size-80 rounded-2xl' />
+        <div className="m-4 flex h-[34rem] w-[22rem] flex-col items-center justify-start gap-5 rounded-2xl bg-white p-6">
+          <img src={QRCode} alt="" className="size-80 rounded-2xl" />
 
-          <p className='text-gray-900 font-bold font-outfit text-2xl'>
+          <p className="font-outfit text-2xl font-bold text-gray-900">
             Improve your front-end skills by building projects
           </p>
 
-          <p className='text-gray-400 font-outfit leading-5 w-72'>          
-            Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+          <p className="w-72 font-outfit leading-5 text-gray-400">
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
           </p>
         </div>
 
         <div className="attribution text-slate-500">
-          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-          Coded by <a href="#">Kaiskii</a>.
+          Challenge by{' '}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by <a href="#">Kaiskii</a>.
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
